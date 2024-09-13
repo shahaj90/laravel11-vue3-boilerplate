0fc659ae09a96f8c95a30fdc681b76364bceeaa6
shahaj90 <gshahaj@gmail.com>
2024-09-13 23:23:03 +0600

    Refresh 404 issue solved

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', function () {
-
+Route::get('/{any}', function () {
     return view('app');
-})->name('application');
+})->where('any', '.*');
